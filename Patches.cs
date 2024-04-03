5b16a41945024e110da06f00d95d6dd0af8f9d45
AzumattDev <azumattgaming@gmail.com>
2024-04-03 04:18:17 -0400

    v1.0.1

--- Patches.cs
+++ Patches.cs
@@ -16,24 +16,8 @@ static class PlayerCustomizerUpdatePatch
             string clipboardText = GUIUtility.systemCopyBuffer;
 
             // Ensure the font supports the characters in the clipboard text
-            if (EnsureFontSupport(__instance.faceText, clipboardText))
-            {
-                // Paste the text from the clipboard
-                __instance.faceText.text = clipboardText;
-            }
+            // Paste the text from the clipboard*/
+            __instance.faceText.text = clipboardText;
         }
-    }
-
-    static bool EnsureFontSupport(TextMeshProUGUI textMesh, string text)
-    {
-        if (FontSupportsCharacters(textMesh.font, text)) return true;
-        PasteFromClipboardPlugin.PasteFromClipboardLogger.LogWarning($"Font does not support characters in clipboard text. Hopefully more characters are supported in the future.");
-        return false;
-    }
-
-
-    static bool FontSupportsCharacters(TMP_FontAsset font, string text)
-    {
-        return text.All(c => font.HasCharacter(c) || char.IsControl(c));
     }
 }
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,3 +1,4 @@
-| `Version` | `Update Notes`    |
-|-----------|-------------------|
-| 1.0.0     | - Initial Release |
+| `Version` | `Update Notes`                                                                                      |
+|-----------|-----------------------------------------------------------------------------------------------------|
+| 1.0.1     | - Simplify the code since the base game will already log the character isn't supported in the font. |
+| 1.0.0     | - Initial Release                                                                                   |
